b749deb94327c516edfb6e9339fe966e4667c77e
JJ Asghar <jj@chef.io>
2017-12-08 12:36:05 -0600

    Initial Commit

---
+++ tirefire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tirefire
+//
+//  Created by JJ Asghar on 12/8/17.
+//  Copyright © 2017 tirefi.re. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tirefire/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tirefire
+//
+//  Created by JJ Asghar on 12/8/17.
+//  Copyright © 2017 tirefi.re. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
